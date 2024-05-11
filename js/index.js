6f93bbf5ac38f49f06bbd47a89f3ca356314425a
Maria <you@example.com>
2024-05-11 14:50:10 -0400

    Fixed: toogle function

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
-const showShareButton = document.getElementById('show-share');
-const hideShareButton = document.getElementById('hide-share');
-const shareSocialMediaSection = document.getElementById('share-social-media');
+const showShareButton = document.getElementById('show-share')
+const hideShareButton = document.getElementById('hide-share')
+const shareSocialMediaSection = document.getElementById('share-social-media')
 
 function toogleShareOnSocialMedia(){
-    console.log(shareSocialMediaSection)
-    shareSocialMediaSection.classList.toogle('share')
+    shareSocialMediaSection.classList.toggle('share')
 }
 
 showShareButton.addEventListener('click', toogleShareOnSocialMedia)
